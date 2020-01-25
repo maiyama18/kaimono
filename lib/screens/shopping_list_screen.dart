0bd757a9a21c8997bc82b36042a1bc8fcda68d4c
maiyama18 <myam0118@gmail.com>
2020-01-25 21:22:34 +0900

    focus to textField when space below list is clicked

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -2,7 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:kaimono/stores/shopping_list.dart';
 import 'package:provider/provider.dart';
 
-class ShoppingListScreen extends StatelessWidget {
+class ShoppingListScreen extends StatefulWidget {
+  @override
+  _ShoppingListScreenState createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends State<ShoppingListScreen> {
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,68 +30,86 @@ class ShoppingListScreen extends StatelessWidget {
       ),
       body: Consumer<ShoppingList>(
         builder: (context, shoppingList, _) {
-          return ListView.separated(
-            separatorBuilder: (context, i) => Container(
-              color: Colors.grey[300],
-              height: 1,
-            ),
-            itemBuilder: (context, i) {
-              if (i == shoppingList.numItems) {
-                return ListTile(
-                  leading: Checkbox(
-                    value: false,
-                    onChanged: (_) {},
-                  ),
-                  title: TextField(
-                    autocorrect: false,
-                    onChanged: (input) => shoppingList.updateInput(input),
-                    onSubmitted: (_) => shoppingList.addToList(),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                );
-              }
-
-              final item = shoppingList.items[i];
-              return Dismissible(
-                key: Key(item.name),
-                background: Container(
-                  color: Colors.red[300],
+          return Column(
+            children: <Widget>[
+              ListView.separated(
+                shrinkWrap: true,
+                separatorBuilder: (context, i) => Container(
+                  color: Colors.grey[300],
+                  height: 1,
                 ),
-                direction: DismissDirection.endToStart,
-                onDismissed: (direction) {
-                  if (direction == DismissDirection.endToStart) {
-                    shoppingList.removeFromList(i);
+                itemBuilder: (context, i) {
+                  if (i == shoppingList.numItems) {
+                    return ListTile(
+                      leading: Checkbox(
+                        value: false,
+                        onChanged: (_) {},
+                      ),
+                      title: TextField(
+                        autocorrect: false,
+                        focusNode: _focusNode,
+                        onChanged: (input) => shoppingList.updateInput(input),
+                        onSubmitted: (_) => shoppingList.addToList(),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    );
                   }
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('「${item.name}」がリストから削除されました'),
-                      action: SnackBarAction(
-                        label: '再追加',
-                        onPressed: shoppingList.undoRemove,
+
+                  final item = shoppingList.items[i];
+                  return Dismissible(
+                    key: Key(item.name),
+                    background: Container(
+                      color: Colors.red[300],
+                    ),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      if (direction == DismissDirection.endToStart) {
+                        shoppingList.removeFromList(i);
+                      }
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('「${item.name}」がリストから削除されました'),
+                          action: SnackBarAction(
+                            label: '再追加',
+                            onPressed: shoppingList.undoRemove,
+                          ),
+                        ),
+                      );
+                    },
+                    child: ListTile(
+                      leading: Checkbox(
+                        value: item.done,
+                        onChanged: (_) {
+                          shoppingList.toggle(i);
+                        },
+                        checkColor: Theme.of(context).toggleableActiveColor,
+                      ),
+                      title: Text(
+                        item.name,
+                        style: TextStyle(
+                          decoration:
+                              item.done ? TextDecoration.lineThrough : null,
+                        ),
                       ),
                     ),
                   );
                 },
-                child: ListTile(
-                  leading: Checkbox(
-                    value: item.done,
-                    onChanged: (_) {
-                      shoppingList.toggle(i);
-                    },
-                    checkColor: Theme.of(context).toggleableActiveColor,
-                  ),
-                  title: Text(
-                    item.name,
-                    style: TextStyle(
-                      decoration: item.done ? TextDecoration.lineThrough : null,
-                    ),
+                itemCount: shoppingList.numItems + 1,
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    print('hello from detector');
+                    FocusScope.of(context).requestFocus(_focusNode);
+                  },
+                  child: Container(
+                    color: Theme.of(context).scaffoldBackgroundColor,
                   ),
                 ),
-              );
-            },
-            itemCount: shoppingList.numItems + 1,
+              )
+            ],
           );
         },
       ),
